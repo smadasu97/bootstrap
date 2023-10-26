334e208263f5249a42bbf598bd8f130793613961
Georgios Vlassis <gvlassis@mailbox.org>
2023-10-26 20:53:19 +0200

    write_crontab

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -30,14 +30,7 @@ printf "Connecting to Atuin\n"
 connect_atuin "gvlassis"
 
 printf "Writing crontab\n"
-printf "
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME} --include {.bash_profile,.bashrc,.blerc,.condarc,.gitconfig,.hushlogin}
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME}/.config/bottom --include bottom.toml
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME}/.config/atuin --include config.toml
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME}/.local/share/atuin --include key
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME}/.config/rclone --include rclone.conf
-* * * * * ${HOME}/.local/bin/rclone bisync --force mydrive:Configs ${HOME}/.ssh --include {id_rsa.pub,id_rsa,id_dsa.pub,id_dsa,id_ecdsa.pub,id_ecdsa,id_ed25519.pub,id_ed25519,config}
-* * * * * /usr/bin/find ${HOME}/Temporary -amin +180 -delete
-" | crontab
+. "${src_path}/write_crontab.sh"
+write_crontab "drive"
 
 printf "Bootstrapping finished🥾\n"
