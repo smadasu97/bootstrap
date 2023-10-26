983c1f11b2ce4587f58db79e68511df5411e7737
Georgios Vlassis <gvlassis@mailbox.org>
2023-10-26 20:42:59 +0200

    Remove debug code

--- src/install_programs.sh
+++ src/install_programs.sh
@@ -35,8 +35,6 @@ function install_programs(){
 
 		printf "Installing catimg\n"
 		${HOME}/.local/bin/goodls -u "${catimg_id}" &> "/dev/null"
-		printf "$PWD\n"
-		ls -al
 		chmod a+x "${PWD}/catimg"
 		cp "${PWD}/catimg" "${HOME}/.local/bin"
 
