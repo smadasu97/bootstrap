13af4f758508e0fe010e178ef114f610e052fae4
Georgios Vlassis <gvlassis@mailbox.org>
2023-10-30 10:36:46 +0100

    Fix Temporary cleaning

--- src/write_crontab.sh
+++ src/write_crontab.sh
@@ -3,6 +3,6 @@ function write_crontab(){
 
 	printf "
 0 * * * * ${HOME}/.local/bin/resync.sh "${name}"
-0,10,20,30,40,50 * * * * /usr/bin/find ${HOME}/Temporary -amin +180 -delete
+0,10,20,30,40,50 * * * * /usr/bin/find ${HOME}/Temporary/* -amin +180 -delete
 	" | crontab
 }
