fcc2150bc0be687d6e45df76dc4b21be3ac97c13
Georgios Vlassis <gvlassis@mailbox.org>
2023-11-16 03:45:13 +0100

    Removed Documents and Mount

--- src/create_dirs.sh
+++ src/create_dirs.sh
@@ -1,9 +1,7 @@
 # Create useful directories
 function create_dirs(){
 	mkdir "${HOME}/Projects" &> "/dev/null"
-	mkdir "${HOME}/Documents" &> "/dev/null"
 	mkdir "${HOME}/Temporary" &> "/dev/null"
-	mkdir "${HOME}/Mount" &> "/dev/null"
 	mkdir "${HOME}/.local" &> "/dev/null"
 	mkdir "${HOME}/.local/bin" &> "/dev/null"
 	mkdir "${HOME}/.local/share" &> "/dev/null"
