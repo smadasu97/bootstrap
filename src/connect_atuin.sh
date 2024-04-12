9b3f062bf20cb39fd6fb709831a40172f9ee336b
Georgios Vlassis <gvlassis@mailbox.org>
2024-04-12 22:47:34 +0200

    Fixed connection to atuin

--- src/connect_atuin.sh
+++ src/connect_atuin.sh
@@ -1,6 +1,6 @@
 function connect_atuin(){
 	local username="${1}"
-	while ! ${HOME}/.local/bin/atuin status &> "/dev/null"; do
+	while ! ${HOME}/.local/bin/atuin status | grep "Remote" &> "/dev/null"; do
 		printf "\e[31mNot logged in Atuin\e[0m\n"
 		read -s -p "password:" password
 		printf "\n"
